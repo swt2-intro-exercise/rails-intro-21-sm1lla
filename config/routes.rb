0b785d861f2a6a9621dff694d27d55fbad33ce61
smilla.fox <smilla.fox@student.hpi.uni-potsdam.de>
2021-11-06 20:17:00 +0100

    Add authors page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 
+  get "/authors/index", to: "authors#index"
+  get "/new_author", to: "authors#new"
+  
+
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   
