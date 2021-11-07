0b1c16ce6e06cd49d12000ffd07fb1e377540ae1
smilla.fox <smilla.fox@student.hpi.uni-potsdam.de>
2021-11-07 10:22:15 +0100

    Update test

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Author, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
   it "should return full name with method name" do
-    author = Author.new(first_name: "Jane", last_name: "Doe")
-    expect(author.name).to eq("Jane Doe")
+    a = Author.new(first_name: "Alan", last_name: "Turing")
+    expect(author.name).to eq("Alan Turing")
   end
 end
