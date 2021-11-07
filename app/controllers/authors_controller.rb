77c6bc351e740ae17e55cbfb428f2add80f1ac19
smilla.fox <smilla.fox@student.hpi.uni-potsdam.de>
2021-11-07 11:04:17 +0100

    Submitting form for author

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -5,4 +5,17 @@ class AuthorsController < ApplicationController
   def new
     @author = Author.new
   end
+
+  def create
+    @author = Author.new(author_params)
+
+    if @author.save
+      redirect_to root_path, notice: 'Success!'
+    end
+  end
+
+  private
+    def author_params
+      params.require(:author).permit(:first_name, :last_name, :homepage)
+    end
 end
