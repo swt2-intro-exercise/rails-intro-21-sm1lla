5c1428c436e4cec264202d745dd2327cc63b715c
smilla.fox <smilla.fox@student.hpi.uni-potsdam.de>
2021-11-07 16:20:19 +0100

    Failing test for #8

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
   it "should return full name with method name" do
     a = Author.new(first_name: "Alan", last_name: "Turing")
     expect(a.name).to eq("Alan Turing")
+  end
+ 
+  it "should only be valid when there is a last name" do
+    @author = Author.new(first_name: "Alan")
+    expect(@author).to_not be_valid
   end
 end
