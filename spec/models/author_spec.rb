150574138ff52197d1893ae9adcea9322e7dbc42
smilla.fox <smilla.fox@student.hpi.uni-potsdam.de>
2021-11-07 10:18:36 +0100

    Add name method to author

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Author, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  it "should return full name with method name" do
+    author = Author.new(first_name: "Jane", last_name: "Doe")
+    expect(author.name).to eq("Jane Doe")
+  end
 end
