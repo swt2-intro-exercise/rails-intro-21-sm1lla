a7bec4e940c592ed8aae0923d27d81b681ac1b51
smilla.fox <smilla.fox@student.hpi.uni-potsdam.de>
2021-11-07 17:20:44 +0100

    Change test for #10

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -16,8 +16,9 @@ describe "Show edit author page", type: :feature do
 
     it "should change displayed information after submitting new information" do
         @alan = FactoryBot.create :author
-        visit edit_author_path(@alan)
+        visit author_path(@alan)
         expect(page).to have_text('Alan Turing', 'http://wikipedia.org/Alan_Turing')
+        visit edit_author_path
         page.fill_in 'author[last_name]', with: 'New'
         page.fill_in 'author[first_name]', with: 'Name'
         page.fill_in 'author[homepage]', with: 'page'
