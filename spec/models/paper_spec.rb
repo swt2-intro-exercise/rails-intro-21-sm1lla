486fd2d30bc081fb2284f9c26fb5d6b990f13803
smilla.fox <smilla.fox@student.hpi.uni-potsdam.de>
2021-11-08 21:26:34 +0100

    add failing test for #14

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should only be valid with a title" do
+    @paper = Paper.new(year: 1950, venue: 'example')
+    expect(@paper).to_not be_valid
+  end
+
+  it "should only be valid with a venue" do
+    @paper = Paper.new(title: 'example', year:1950)
+    expect(@paper).to_not be_valid
+  end
+
+  it "should only be valid with a year which should be a numeric value" do
+    @paper = Paper.new(title: 'example', venue: 'test')
+    expect(@paper).to_not be_valid
+    @paper.year = "test"
+    expect(@paper).to_not be_valid
+  end
+
 end
