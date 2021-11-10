06eb32a33555ab87a0773c0a4bd8ec73b4a6c081
smilla.fox <smilla.fox@student.hpi.uni-potsdam.de>
2021-11-10 20:25:53 +0100

    Add association between Author and Paper

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,7 @@
 class Author < ApplicationRecord
     validates :last_name, presence: true
+
+    has_and_belongs_to_many :papers
     
     def name
         self.first_name + " " + self.last_name
--- app/models/paper.rb
+++ app/models/paper.rb
@@ -2,4 +2,6 @@ class Paper < ApplicationRecord
     validates :title, presence: true
     validates :year, presence: true, numericality: true
     validates :venue, presence: true
+    
+    has_and_belongs_to_many :authors
 end
