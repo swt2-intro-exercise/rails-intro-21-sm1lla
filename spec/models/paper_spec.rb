18f3951f8e3770f1e6a44a5fee18d6919df5f9d5
smilla.fox <smilla.fox@student.hpi.uni-potsdam.de>
2021-11-10 20:19:47 +0100

    Failing test for #15

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -18,4 +18,9 @@ RSpec.describe Paper, type: :model do
     expect(@paper).to_not be_valid
   end
 
+  it "should have an empty list of authors" do
+    @paper = Paper.new()
+    expect(@paper.authors).to be_empty
+  end
+
 end
